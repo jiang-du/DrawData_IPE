52805d0fc41f2f339b8fc55f5cf16edcfb3057d2
Jiang Du <jiangdu.xidian@outlook.com>
2020-07-24 22:03:46 +0800

    实现了对关注人的阴影视频特效

--- draw.cpp
+++ draw.cpp
@@ -13,8 +13,14 @@ int main()
     cv::Mat image;
     cv::VideoCapture capture;
     capture.open(videoname);
+#if SAVEVIDEO
+    double fps = 25;
+    cv::Size videoSize(1920, 1080);
+    cv::VideoWriter writer("output.mp4", cv::VideoWriter::fourcc('X', '2', '6', '4'), fps, videoSize);
+#endif
     if (capture.isOpened())
     {
+        int count_frame = 0;
         while (1)
         {
             capture >> image;
@@ -26,19 +32,78 @@ int main()
                 cv::waitKey(0);
                 break;
             }
-            for (int i = 0; i < 5;)
+            for (int i = 0; i < 5; i++)
             {
-                short *pdata = data + (i++) * 4;
+                short *pdata = data + i * 4;
+
+#if (DISP_METHOD == 1)
+                // Method 1: 直接显示框
                 // 定义矩形框
                 cv::Rect rect(*pdata, *(pdata + 1), *(pdata + 2) - *pdata, *(pdata + 3) - *(pdata + 1));
                 // 绘制框
-                cv::rectangle(image, rect, cv::Scalar(128, 255, 128, (double)0.5), 5, 8, 0);
+                v::rectangle(image, rect, cv::Scalar(64 + i * 48, 128 + i * 32, 192 - i * 48, (double)0.5), 5, 8, 0);
+                // image.at<u_char>(0,0,0) = 1;
+#else
+#if (DISP_METHOD == 2)
+                // Method 2: 画圆
+                // 获取框的中心点
+                short cx = (*pdata + *(pdata + 2)) / 2;
+                short cy = (*(pdata + 1) + *(pdata + 3)) / 2;
+                // 获取圆的半径
+                double radius = 0.5 * Eclidian(*(pdata + 2) - *pdata, *(pdata + 3) - *(pdata + 1));
+                cv::circle(image, cv::Point(cx, cy), radius, cv::Scalar(0, 255, 0), 3);
+#else
+#if (DISP_METHOD == 3)
+
+                // Method 3: Attention
+                if ((i - 3) != 0)
+                    // 选择需要关注的person ID
+                    continue;
+                // 获取框的中心点
+                short cx = (*pdata + *(pdata + 2)) / 2;
+                short cy = (*(pdata + 1) + *(pdata + 3)) / 2;
+                // 获取圆的半径
+                double radius = 0.5 * Eclidian(*(pdata + 2) - *pdata, *(pdata + 3) - *(pdata + 1));
+                // 对图像进行逐像素处理
+                //遍历所有像素，并设置像素值
+                for (int xi = 0; xi < image.rows;)
+                {
+                    //获取第i行首像素指针
+                    cv::Vec3b *p = image.ptr<cv::Vec3b>(xi++);
+                    for (int yi = 0; yi < image.cols; ++yi)
+                    {
+                        // 设置倍率
+                        float dist = Eclidian(xi - cy, yi - cx);
+                        float rate = MIN(3.5 - 3 * MIN(dist / radius, 1), 1);
+                        // 对内存地址的数据直接进行修改
+                        for (int _channel = 0; _channel < 3; ++_channel)
+                            // BGR
+                            p[yi][_channel] = (unsigned char)(rate * (float)p[yi][_channel]);
+                    }
+                }
+                break;
+#endif
+#endif
+#endif
             }
             imshow("Intelligent Physical Education System", image);
+#if SAVEVIDEO
+            writer << image;
+#endif
             if (cv::waitKey(1) >= 0)
             {
                 fclose(fp);
                 break;
+            }
+            count_frame++;
+            
+            if (!(count_frame % 5))
+            {
+                printf("Processing %5.2f\%, %4d / %4d [", (100 * (float)count_frame / TOTAL_NUM_FRAME), count_frame, TOTAL_NUM_FRAME);
+                for (int _t = 0; _t++ < (40 * (float)count_frame / TOTAL_NUM_FRAME); printf("="));
+                printf(">");
+                for (int _t = 0; _t++ < 40 - (40 * (float)count_frame / TOTAL_NUM_FRAME); printf(" "));
+                printf("]\n");
             }
         }
     }
--- draw.h
+++ draw.h
@@ -1,15 +1,22 @@
 #ifndef DRAW_H
 #define DRAW_H
 
+// 是否保存视频文件输出
+#define SAVEVIDEO 1 // 设置成0就不保存，默认为1
+#define TOTAL_NUM_FRAME 1097
+#define DISP_METHOD 3
 // 设置文件路径
 #define FILENAME "RawData.bin"
 #define VIDEONAME "./../datasets/ori.avi"
 
 #include <stdio.h>
+#include <math.h>
+
+// 定义欧式距离公式，返回值默认double类型。
+#define Eclidian(x, y) sqrt((double)((x) * (x) + (y) * (y)))
 
 // 读取二进制数据的函数
 int readData(short int *data, FILE *fp);
-
 
 int readData(short int *data, FILE *fp)
 {
